d304bbf92fec0711f3b8d19a8fe86a6f398c3000
yezi1 <13719490809@163.com>
2018-03-23 07:54:38 +0800

    5

---
+++ awesomepos/src/components/page/Pos.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="pos">
+		<div>
+			<el-row>
+				<el-col :span='7'>
+					<el-tabs>
+						<el-tab-pane label="点餐">
+							点餐
+						</el-tab-pane>
+						<el-tab-pane label="挂单">
+							挂单
+						</el-tab-pane>
+						<el-tab-pane label="外卖">
+							外卖
+						</el-tab-pane>
+					</el-tabs>
+				</el-col>
+				<!--商品展示-->
+				<el-col :span="17">
+					我是产品栏
+				</el-col>
+			</el-row>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Pos',
+		data() {
+			return {}
+		}
+	}
+</script>
+<style scoped>
+
+</style>
